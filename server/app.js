bb625b9e4832d18e8422bba5255aa768ad409a22
stas <stas.sklyar.dev@gmail.com>
2024-04-04 08:38:00 -0700

    in progress

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Express app!')
